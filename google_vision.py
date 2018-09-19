d19858a1fd710838f35debac1671c9a77073bf96
qzhizhou@bu.edu <475084977@qq.com>
2018-09-19 11:00:37 -0400

    add the .rename code

--- google_vision.py
+++ google_vision.py
@@ -13,6 +13,10 @@ from google.cloud.vision import types
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 
+# set the style of words
+font = ImageFont.truetype("/Users/zhizhouqiu/Desktop/EC601/mini_project1/Arial.ttf", 30)
+
+
 for file in os.listdir(): #file is the image name of current_dir
     if file.endswith(".jpg"):
         pic = file #image path + name +label
@@ -28,84 +32,57 @@ for file in os.listdir(): #file is the image name of current_dir
         response = client.label_detection(image=image)
         labels = response.label_annotations
 
-        #print labels in every picture
+        # print labels in every picture
         print(pic)
+        label_list = [] # creat a list to store labels 
         for label in labels:
             print(label.description)
+            label_list.append(label.description)
+        print('\n')
+        print(label_list)
         print('\n')
 
+        # start to draw
+        a=''
+        c=''
+        imageFile = pic
+        im = Image.open(imageFile)
 
-# set the style of words
-font = ImageFont.truetype("/Users/zhizhouqiu/Desktop/EC601/mini_project1/Arial.ttf", 50)
+        # draw
+        draw = ImageDraw.Draw(im)
+        if len(label_list) < 6:
+	        for i in range(0, len(label_list)):
+		        a = label_list[i] + a + '/ '
+	        draw.text((0, 30), a, (50, 0, 0), font=font)
+        else:
+	        for i in range(0, 6):
+		        a = label_list[i] + '/ ' + a
+	        draw.text((0, 30), a, (50, 0, 0), font=font)  # word location/content/color/typeface
+	        for i in range(7, len(label_list)):
+		        c = label_list[i] + '/ ' + c
+	        draw.text((0, 100), c, (50, 0, 0), font=font)
+        im.save(pic)
 
-# open images/image1
-imageFile = "1.jpg"
-im = Image.open(imageFile)
-# draw  The text needs to be added yourself.
-draw = ImageDraw.Draw(im)
-draw.text((0, 30), 'green grass football lawn pallone artificial turf', (50, 0, 0), font=font)    #word location/content/color/typeface
-# save images
-im.save("1_target.jpg")
+#Rename all the .jpg files
+class ImageRename():
+    def __init__(self):
+        self.path = '/Users/zhizhouqiu/desktop/ec601/mini_project1/code'
+    
+    def rename(self):
+        filelist = os.listdir(self.path)
+        total_num = len(filelist)
+        
+        i = 0
+        
+        for item in filelist:
+            if item.endswith('.jpg'):
+                src = os.path.join(os.path.abspath(self.path), item)
+                dst = os.path.join(str(i) + '.jpg')
+                os.rename(src, dst)
+                
+                i = i + 1
 
-#image2
-imageFile = "2.jpg"
-im = Image.open(imageFile)
-draw = ImageDraw.Draw(im)
-draw.text((0, 30), 'hand finger fashion accessory joint arm wrist girl', (50, 0, 0), font=font)
-im.save("2_target.jpg")
 
-#image3
-imageFile = "3.jpg"
-im = Image.open(imageFile)
-draw = ImageDraw.Draw(im)
-draw.text((0, 30), 'sport gridiron,football,player,tackle', (50, 0, 0), font=font)
-draw.text((0, 100), 'protective equipment in gridiron football', (50, 0, 0), font=font)
-im.save("3_target.jpg")
-
-#image4
-imageFile = "4.jpg"
-im = Image.open(imageFile)
-draw = ImageDraw.Draw(im)
-draw.text((0, 30), 'baseball,player,bat ball games', (50, 0, 0), font=font)
-draw.text((0, 100), 'baseball equipment,college baseball,baseball positions', (50, 0, 0), font=font)
-im.save("4_target.jpg")
-
-#image5
-imageFile = "5.jpg"
-im = Image.open(imageFile)
-draw = ImageDraw.Draw(im)
-draw.text((0, 30), 'ski pole,mountain range,skiing,freestyle skiing', (50, 0, 0), font=font)
-draw.text((0, 100), 'piste,extreme sport,winter sport,ski equipment', (50, 0, 0), font=font)
-im.save("5_target.jpg")
-
-#image6
-imageFile = "6.jpg"
-im = Image.open(imageFile)
-draw = ImageDraw.Draw(im)
-draw.text((0, 30), 'sky,cloud,extreme sport,adventure,sunrise,rock', (50, 0, 0), font=font)
-draw.text((0, 100), 'sport climbing,jumping,recreation,silhouette', (50, 0, 0), font=font)
-im.save("6_target.jpg")
-
-#image7
-imageFile = "7.jpg"
-im = Image.open(imageFile)
-draw = ImageDraw.Draw(im)
-draw.text((0, 30), 'blue,figure skating,figure skate,ice skating,skating', (50, 0, 0), font=font)
-draw.text((0, 100), 'figure skater,gymnast,winter sport,recreation,girl', (50, 0, 0), font=font)
-im.save("7_target.jpg")
-
-#image8
-imageFile = "8.jpg"
-im = Image.open(imageFile)
-draw = ImageDraw.Draw(im)
-draw.text((0, 30), 'player,football player,sport venue,soccer player', (50, 0, 0), font=font)
-draw.text((0, 100), 'grass,competition event,ball game,forward,team sport', (50, 0, 0), font=font)
-im.save("8_target.jpg")
-
-#image9
-imageFile = "9.jpg"
-im = Image.open(imageFile)
-draw = ImageDraw.Draw(im)
-draw.text((0, 30), 'trophy,award,close up,macro photography', (50, 0, 0), font=font)
-draw.text((0, 100), 'computer wallpaper', (50, 0, 0), font=font)
-im.save("9_target.jpg")
+if __name__ == '__main__':
+    newname = ImageRename()
+    newname.rename()
